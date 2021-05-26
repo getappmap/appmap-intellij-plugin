f069dd60fed75f46ee33b15bb01bf90fb72978a4
J. Ansorg <github@ja-dev.eu>
2021-05-26 10:01:57 +0200

    Add new action to open the AppMap documentation (#34)

--- src/main/java/appland/Icons.java
+++ src/main/java/appland/Icons.java
@@ -7,6 +7,7 @@ import javax.swing.*;
 public class Icons {
     // 16x16
     public static final Icon APPMAP_FILE = IconLoader.getIcon("/icons/appmap.svg", Icons.class);
+    public static final Icon APPMAP_DOCS = APPMAP_FILE;
 
     // 13x13
     public static final Icon APPMAP_FILE_SMALL = IconLoader.getIcon("/icons/appmap_small.svg", Icons.class);
