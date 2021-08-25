e747ecc93f7601de85c7fa477ad163837b16278b
Joachim Ansorg <github@ja-dev.eu>
2021-08-25 19:41:49 +0200

    Don't run startup activity in unit test mode

--- src/main/java/appland/startup/AppLandStartupActivity.java
+++ src/main/java/appland/startup/AppLandStartupActivity.java
@@ -3,6 +3,7 @@ package appland.startup;
 import appland.milestones.MilestonesViewType;
 import appland.milestones.UserMilestonesEditorProvider;
 import appland.settings.AppMapApplicationSettingsService;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.startup.StartupActivity;
 import com.intellij.openapi.startup.StartupManager;
@@ -11,7 +12,8 @@ import org.jetbrains.annotations.NotNull;
 public class AppLandStartupActivity implements StartupActivity {
     @Override
     public void runActivity(@NotNull Project project) {
-        if (AppMapApplicationSettingsService.getInstance().isFirstStart()) {
+        boolean unitTestMode = ApplicationManager.getApplication().isUnitTestMode();
+        if (AppMapApplicationSettingsService.getInstance().isFirstStart() && !unitTestMode) {
             AppMapApplicationSettingsService.getInstance().setFirstStart(false);
 
             openToolWindowAndQuickstart(project);
