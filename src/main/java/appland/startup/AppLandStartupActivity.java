aaf3f8f949ee5d8add041731ee8708e5426dbe29
Joachim Ansorg <github@ja-dev.eu>
2021-08-25 19:21:01 +0200

    Don't open AppLand tool window at first startup

--- src/main/java/appland/startup/AppLandStartupActivity.java
+++ src/main/java/appland/startup/AppLandStartupActivity.java
@@ -3,11 +3,9 @@ package appland.startup;
 import appland.milestones.MilestonesViewType;
 import appland.milestones.UserMilestonesEditorProvider;
 import appland.settings.AppMapApplicationSettingsService;
-import appland.toolwindow.AppMapToolWindowFactory;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.startup.StartupActivity;
 import com.intellij.openapi.startup.StartupManager;
-import com.intellij.openapi.wm.ToolWindowManager;
 import org.jetbrains.annotations.NotNull;
 
 public class AppLandStartupActivity implements StartupActivity {
@@ -22,12 +20,7 @@ public class AppLandStartupActivity implements StartupActivity {
 
     static void openToolWindowAndQuickstart(@NotNull Project project) {
         StartupManager.getInstance(project).runWhenProjectIsInitialized(() -> {
-            var window = ToolWindowManager.getInstance(project).getToolWindow(AppMapToolWindowFactory.TOOL_WINDOW_ID);
-            if (window != null) {
-                window.show(() -> {
-                    UserMilestonesEditorProvider.open(project, MilestonesViewType.Welcome);
-                });
-            }
+            UserMilestonesEditorProvider.open(project, MilestonesViewType.Welcome);
         });
     }
 }
