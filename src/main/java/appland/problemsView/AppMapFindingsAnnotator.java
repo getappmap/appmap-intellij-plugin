f169c138737166784bc5ebac93943f97a00aa2ab
Joachim Ansorg <github@ja-dev.eu>
2022-09-01 12:37:29 +0200

    feat: add new tab to the problems view tool window, support AppMap findings

---
+++ src/main/java/appland/problemsView/AppMapFindingsAnnotator.java
@@ -0,0 +1,58 @@
+package appland.problemsView;
+
+import com.intellij.lang.annotation.AnnotationHolder;
+import com.intellij.lang.annotation.Annotator;
+import com.intellij.lang.annotation.HighlightSeverity;
+import com.intellij.openapi.progress.ProgressManager;
+import com.intellij.openapi.util.TextRange;
+import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.psi.PsiDocumentManager;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Annotator, which adds highlighting of detected problems to editors.
+ */
+public class AppMapFindingsAnnotator implements Annotator {
+    @Override
+    public void annotate(@NotNull PsiElement element, @NotNull AnnotationHolder holder) {
+        if (element instanceof PsiFile && element.isValid()) {
+            ProgressManager.checkCanceled();
+
+            var project = element.getProject();
+
+            // we're only handling committed PsiFiles because we have to match the problem range with the PSI tree
+            var psiManager = PsiDocumentManager.getInstance(project);
+            var document = psiManager.getDocument((PsiFile) element);
+            if (document == null || psiManager.isUncommited(document)) {
+                return;
+            }
+
+            var file = ((PsiFile) element).getVirtualFile();
+            if (file == null || !file.isValid()) {
+                return;
+            }
+
+            var maxLineCount = document.getLineCount();
+            for (var problem : FindingsManager.getInstance(project).getScannerProblems(file)) {
+                ProgressManager.checkCanceled();
+
+                var line = problem.getLine();
+                if (line >= 0 && line < maxLineCount) {
+                    var lineStart = document.getLineStartOffset(line);
+                    var lineEnd = document.getLineEndOffset(line);
+                    var lineText = document.getText(TextRange.create(lineStart, lineEnd));
+                    var emptyLinePrefixLength = lineText.length() - StringUtil.trimLeading(lineText).length();
+
+                    lineStart += emptyLinePrefixLength;
+                    if (lineStart < lineEnd) {
+                        holder.newAnnotation(HighlightSeverity.ERROR, problem.getText())
+                                .range(TextRange.create(lineStart, lineEnd))
+                                .create();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/test/java/appland/problemsView/FindingsManagerTest.java
@@ -0,0 +1,83 @@
+package appland.problemsView;
+
+import appland.AppMapBaseTest;
+import appland.problemsView.listener.ScannerFindingsListener;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+public class FindingsManagerTest extends AppMapBaseTest {
+    @Before
+    @After
+    public void resetFindings() {
+        TestFindingsManager.getInstance(getProject()).reset();
+
+        var manager = FindingsManager.getInstance(getProject());
+        assertEquals(0, manager.getProblemFileCount());
+        assertEquals(0, manager.getProblemCount());
+    }
+
+    @Test
+    public void fileNames() {
+        assertTrue(FindingsManager.isFindingFile("/parent/child/appmap-findings.json"));
+        assertTrue(FindingsManager.isFindingFile("appmap-findings.json"));
+
+        assertFalse(FindingsManager.isFindingFile("/parent/child/file.json"));
+        assertFalse(FindingsManager.isFindingFile("/parent/child/appmap-findings.yaml"));
+    }
+
+    @Test
+    public void findingsVSCodeSystem() {
+        var manager = FindingsManager.getInstance(getProject());
+        var root = myFixture.copyDirectoryToProject("vscode/workspaces/project-system", "root");
+
+        var findingsFile = root.findChild("appmap-findings.json");
+        assertNotNull(findingsFile);
+
+        var problematicFile = root.findFileByRelativePath("app/controllers/microposts_controller.rb");
+        assertNotNull(problematicFile);
+
+        manager.loadAllFindingFiles();
+
+        assertEquals(1, manager.getProblemFileCount());
+        assertEquals(1, manager.getProblemCount());
+        assertEquals(List.of(problematicFile), manager.getProblemFiles());
+        assertEquals(1, manager.getProblemCount(problematicFile));
+        assertEquals(0, manager.getOtherProblemCount());
+        assertEquals(0, manager.getOtherProblems().size());
+
+        // removing the findings file must update the problems
+        manager.removeFindingsFile(findingsFile.getPath());
+        assertEquals(0, manager.getProblemFileCount());
+        assertEquals(0, manager.getProblemCount());
+        assertEquals(0, manager.getProblemFiles().size());
+        assertEquals(0, manager.getProblemCount(problematicFile));
+        assertEquals(0, manager.getOtherProblemCount());
+        assertEquals(0, manager.getOtherProblems().size());
+    }
+
+    @Test
+    public void findingsFileWatcher() throws InterruptedException {
+        var manager = FindingsManager.getInstance(getProject());
+
+        var condition = createAppMapFindingsCondition();
+        // adding an appmap-findings.json file must trigger a refresh via the file watcher
+        myFixture.copyDirectoryToProject("vscode/workspaces/project-system", "root");
+        assertTrue(condition.await(10, TimeUnit.SECONDS));
+
+        assertEquals(1, manager.getProblemFileCount());
+        assertEquals(1, manager.getProblemCount());
+    }
+
+    private CountDownLatch createAppMapFindingsCondition() {
+        var latch = new CountDownLatch(1);
+        getProject().getMessageBus().connect(getTestRootDisposable()).subscribe(ScannerFindingsListener.TOPIC, project -> {
+            latch.countDown();
+        });
+        return latch;
+    }
+}
