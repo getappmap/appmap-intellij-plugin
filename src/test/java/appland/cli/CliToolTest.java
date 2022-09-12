88382162181cf1d0e27bae79bd89a5381cf31aa3
jansorg <github@joachim-ansorg.de>
2022-09-12 14:11:04 +0200

    fix: CLI download URL on Windows

--- src/test/java/appland/cli/CliToolTest.java
+++ src/test/java/appland/cli/CliToolTest.java
@@ -11,6 +11,9 @@ public class CliToolTest extends AppMapBaseTest {
 
         assertEquals("https://github.com/applandinc/appmap-js/releases/download/%40appland%2Fappmap-v3.32.2/appmap-macos-arm64",
                 CliTool.AppMap.getDownloadUrl("3.32.2", "macos", "arm64"));
+
+        assertEquals("https://github.com/applandinc/appmap-js/releases/download/%40appland%2Fappmap-v3.32.2/appmap-win-x64.exe",
+                CliTool.AppMap.getDownloadUrl("3.32.2", "win", "x64"));
     }
 
     @Test
@@ -20,5 +23,19 @@ public class CliToolTest extends AppMapBaseTest {
 
         assertEquals("https://github.com/applandinc/appmap-js/releases/download/%40appland%2Fscanner-v1.67.0/scanner-macos-arm64",
                 CliTool.Scanner.getDownloadUrl("1.67.0", "macos", "arm64"));
+
+        assertEquals("https://github.com/applandinc/appmap-js/releases/download/%40appland%2Fscanner-v1.67.0/scanner-win-x64.exe",
+                CliTool.Scanner.getDownloadUrl("1.67.0", "win", "x64"));
+    }
+
+    @Test
+    public void binaryNames() {
+        assertEquals("appmap-linux-x64", CliTool.AppMap.getBinaryName("linux", "x64"));
+        assertEquals("appmap-macos-x64", CliTool.AppMap.getBinaryName("macos", "x64"));
+        assertEquals("appmap-win-x64.exe", CliTool.AppMap.getBinaryName("win", "x64"));
+
+        assertEquals("scanner-linux-x64", CliTool.Scanner.getBinaryName("linux", "x64"));
+        assertEquals("scanner-macos-x64", CliTool.Scanner.getBinaryName("macos", "x64"));
+        assertEquals("scanner-win-x64.exe", CliTool.Scanner.getBinaryName("win", "x64"));
     }
 }
