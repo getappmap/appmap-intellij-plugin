c593fa50e218d28b619e9645f106b8f2e217eb77
Joachim Ansorg <github@ja-dev.eu>
2022-09-19 16:25:14 +0200

    fix: AppMap Java line's are 1-based

---
+++ src/test/java/appland/problemsView/ScannerProblemTest.java
@@ -0,0 +1,22 @@
+package appland.problemsView;
+
+import appland.AppMapBaseTest;
+import appland.problemsView.model.ScannerFinding;
+import com.intellij.testFramework.LightVirtualFile;
+import org.junit.Test;
+
+import java.util.List;
+
+public class ScannerProblemTest extends AppMapBaseTest {
+    @Test
+    public void zeroBasedLine() {
+        var scannerFinding = new ScannerFinding();
+        scannerFinding.stack = List.of("parent/child/package/file.java:100");
+        var problemLocation = scannerFinding.getProblemLocation();
+        assertNotNull(problemLocation);
+        assertEquals(Integer.valueOf(100), problemLocation.line);
+
+        var scannerProblem = new ScannerProblem(FindingsManager.getInstance(getProject()), new LightVirtualFile(), scannerFinding);
+        assertEquals("IntelliJ's line is 0-based", 99, scannerProblem.getLine());
+    }
+}
