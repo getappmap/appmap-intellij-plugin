fc2701a26fd85f45ee582644ea361c4c86c95c9d
Joachim Ansorg <github@ja-dev.eu>
2022-10-11 15:34:53 +0200

    fix: don't show a JDK settings link in 2021.3, because it's not supported everywhere

--- plugin-core/src/main/java/appland/execution/UnsupportedJdkException.java
+++ plugin-core/src/main/java/appland/execution/UnsupportedJdkException.java
@@ -4,6 +4,7 @@ import appland.AppMapBundle;
 import com.intellij.execution.ExecutionException;
 import com.intellij.notification.Notification;
 import com.intellij.notification.NotificationListener;
+import com.intellij.openapi.application.ApplicationInfo;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.ui.configuration.ProjectSettingsService;
 import org.jetbrains.annotations.NotNull;
@@ -15,7 +16,7 @@ public class UnsupportedJdkException extends ExecutionException implements Hyper
     private final Project project;
 
     public UnsupportedJdkException(@NotNull Project project) {
-        super(AppMapBundle.get("appMapExecutor.jdkNotSupported"));
+        super(findMessage());
         this.project = project;
     }
 
@@ -29,5 +30,14 @@ public class UnsupportedJdkException extends ExecutionException implements Hyper
     @Override
     public void hyperlinkUpdate(@NotNull Notification notification, @NotNull HyperlinkEvent event) {
         hyperlinkUpdate(event);
+    }
+
+    @NotNull
+    private static String findMessage() {
+        // 2021.3 and earlier don't support link handlers of ExecutionException when the exception is shown as message dialog.
+        // 2022.1 and later display a notification instead of a message dialog and thus support the link handler.
+        return ApplicationInfo.getInstance().getBuild().getBaselineVersion() <= 213
+                ? AppMapBundle.get("appMapExecutor.jdkNotSupported")
+                : AppMapBundle.get("appMapExecutor.jdkNotSupportedWithLink");
     }
 }
