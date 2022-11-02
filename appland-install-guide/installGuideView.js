c4a5d40d07e12439e732e1e38a417e9aa9e15e99
Joachim Ansorg <github@ja-dev.eu>
2022-11-02 11:21:21 +0100

    chore: sync props handling of installGuideView.js with VSCode, add handlers for "perform-auth" and "settings"

--- appland-install-guide/installGuideView.js
+++ appland-install-guide/installGuideView.js
@@ -5,103 +5,127 @@ import MessagePublisher from './messagePublisher';
 import vscode from './vsCodeBridge';
 
 export function mountInstallGuide() {
-    const messages = new MessagePublisher(vscode);
+  const messages = new MessagePublisher(vscode);
 
-    messages.on('init', ({projects: startProjects, page: startPage, disabled, findingsEnabled}) => {
-        let currentPage = startPage;
-        let currentProject;
+  messages.on('init', (initialData) => {
+    let currentPage = initialData.page;
+    let currentProject;
 
-        const app = new Vue({
-            el: '#app',
-            render(h) {
-                return h(VInstallGuide, {
-                    ref: 'ui',
-                    props: {
-                        projects: this.projects,
-                        disabledPages: new Set(disabled),
-                        editor: 'jetbrains',
-                        findingsEnabled,
-                        featureFlags: new Set(['disable-record-pending'])
-                    },
-                });
-            },
-            data() {
-                return {
-                    projects: startProjects,
-                };
-            },
-            beforeCreate() {
-                this.$on('open-page', async (pageId) => {
-                    // Wait until next frame if there's no current project. It may take some time for the
-                    // view to catch up.
-                    if (!currentProject) await new Promise((resolve) => requestAnimationFrame(resolve));
-
-                    currentPage = pageId;
-                    vscode.postMessage({
-                        command: 'open-page',
-                        page: currentPage,
-                        project: currentProject,
-                    });
-                });
-            },
-            mounted() {
-                this.$refs.ui.jumpTo(startPage);
-
-                document.addEventListener('click', (e) => {
-                    if (e && e.target) {
-                        const href = e.target.href;
-                        if (href && (href.startsWith("http://") || href.startsWith("https://"))) {
-                            e.preventDefault();
-                            vscode.postMessage({command: 'click-link', uri: e.target.href});
-                            return false;
-                        }
-                    }
-                });
-            },
+    const app = new Vue({
+      el: '#app',
+      render(h) {
+        return h(VInstallGuide, {
+          ref: 'ui',
+          props: {
+            projects: this.projects,
+            editor: 'jetbrains',
+            disabledPages: new Set(this.disabledPages),
+            featureFlags: new Set(['disable-record-pending']),
+            analysisEnabled: this.analysisEnabled,
+            findingsEnabled: this.findingsEnabled,
+            userAuthenticated: this.userAuthenticated,
+          },
         });
+      },
+      data() {
+        return {
+          projects: initialData.projects,
+          disabledPages: initialData.disabledPages,
+          analysisEnabled: initialData.analysisEnabled,
+          findingsEnabled: initialData.findingsEnabled,
+          userAuthenticated: initialData.userAuthenticated,
+        };
+      },
+      beforeCreate() {
+        this.$on('open-page', async (pageId) => {
+          // Wait until next frame if there's no current project. It may take some time for the
+          // view to catch up.
+          if (!currentProject) await new Promise((resolve) => requestAnimationFrame(resolve));
 
-        app.$on('clipboard', (text) => {
-            vscode.postMessage({
-                command: 'clipboard',
-                page: currentPage,
-                project: currentProject,
-                text,
-            });
+          currentPage = pageId;
+          vscode.postMessage({
+            command: 'open-page',
+            page: currentPage,
+            project: currentProject,
+          });
         });
+      },
+      mounted() {
+        this.$refs.ui.jumpTo(initialData.page);
 
-        app.$on('select-project', (project) => {
-            currentProject = project;
+        document.addEventListener('click', (e) => {
+          if (e && e.target) {
+            const href = e.target.href;
+            if (href && (href.startsWith("http://") || href.startsWith("https://"))) {
+              e.preventDefault();
+              vscode.postMessage({command: 'click-link', uri: e.target.href});
+              return false;
+            }
+          }
         });
-
-        app.$on('view-problems', (projectPath) => {
-            vscode.postMessage({command: 'view-problems', projectPath});
-        });
-
-        app.$on('openAppmap', (file) => {
-            vscode.postMessage({command: 'open-file', file});
-        });
-
-        app.$on('perform-install', (path, language) => {
-          vscode.postMessage({ command: 'perform-install', path, language });
-        });
-
-        app.$on('open-instruction', (pageId) => {
-            app.$refs.ui.jumpTo(pageId);
-        });
-
-        app.$on('generate-openapi', (projectPath) => {
-            messages.rpc('generate-openapi', {projectPath});
-        });
-
-        messages.on('page', ({page}) => {
-            app.$refs.ui.jumpTo(page);
-        });
-
-        messages.on('projects', ({projects}) => {
-            app.projects = projects;
-            app.$forceUpdate();
-        });
+      },
     });
 
-    vscode.postMessage({command: 'ready'});
+    app.$on('clipboard', (text) => {
+      vscode.postMessage({
+        command: 'clipboard',
+        page: currentPage,
+        project: currentProject,
+        text,
+      });
+    });
+
+    app.$on('select-project', (project) => {
+      currentProject = project;
+    });
+
+    app.$on('view-problems', (projectPath) => {
+      vscode.postMessage({command: 'view-problems', projectPath});
+    });
+
+    app.$on('openAppmap', (file) => {
+      vscode.postMessage({command: 'open-file', file});
+    });
+
+    app.$on('perform-install', (path, language) => {
+      vscode.postMessage({command: 'perform-install', path, language});
+    });
+
+    app.$on('open-instruction', (pageId) => {
+      app.$refs.ui.jumpTo(pageId);
+    });
+
+    app.$on('generate-openapi', (projectPath) => {
+      messages.rpc('generate-openapi', {projectPath});
+    });
+
+    app.$on('perform-auth', () => {
+      vscode.postMessage({command: 'perform-auth'});
+    });
+
+    // listeners for messages sent by the plugin
+    messages.on('page', ({page}) => {
+      app.$refs.ui.jumpTo(page);
+    });
+
+    messages.on('projects', ({projects}) => {
+      app.projects = projects;
+      app.$forceUpdate();
+    });
+
+    messages.on('settings', ({userAuthenticated, analysisEnabled, findingsEnabled}) => {
+      if (userAuthenticated !== undefined) {
+        app.userAuthenticated = userAuthenticated;
+      }
+      if (analysisEnabled !== undefined) {
+        app.analysisEnabled = analysisEnabled;
+      }
+      if (findingsEnabled !== undefined) {
+        app.findingsEnabled = findingsEnabled;
+      }
+      app.$forceUpdate();
+    });
+  });
+
+  vscode.postMessage({command: 'ready'});
 }
