dc9225e56f2d252989f29d7f355765f779387465
Joachim Ansorg <github@ja-dev.eu>
2022-11-14 12:57:31 +0100

    feat: send analysis:enable and analysis:disable events

--- plugin-core/src/main/java/appland/startup/AppLandStartupActivity.java
+++ plugin-core/src/main/java/appland/startup/AppLandStartupActivity.java
@@ -1,22 +1,27 @@
 package appland.startup;
 
+import appland.AppLandLifecycleService;
 import appland.installGuide.InstallGuideEditorProvider;
 import appland.installGuide.InstallGuideViewPage;
 import appland.problemsView.FindingsManager;
 import appland.settings.AppMapApplicationSettingsService;
+import appland.settings.AppMapSettingsListener;
 import appland.telemetry.TelemetryService;
-
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.startup.StartupActivity;
 import com.intellij.openapi.startup.StartupManager;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class AppLandStartupActivity implements StartupActivity {
     @Override
     public void runActivity(@NotNull Project project) {
         // load initial findings of the project
         FindingsManager.getInstance(project).reloadAsync();
+
+        registerFindingsTelemetryListener(project);
 
         // show AppMap intro at first start
         var unitTestMode = ApplicationManager.getApplication().isUnitTestMode();
@@ -29,6 +34,32 @@ public class AppLandStartupActivity implements StartupActivity {
             telemetry.notifyTelemetryUsage(project);
             telemetry.sendEvent("plugin:install");
         }
+    }
+
+    private void registerFindingsTelemetryListener(@NotNull Project project) {
+        project.getMessageBus()
+                .connect(AppLandLifecycleService.getInstance(project))
+                .subscribe(AppMapSettingsListener.TOPIC, new AppMapSettingsListener() {
+                    private final AtomicBoolean lastEnabledState = new AtomicBoolean(AppMapApplicationSettingsService.getInstance().isAnalysisEnabled());
+
+                    @Override
+                    public void apiKeyChanged() {
+                        sendTelemetryOnChange();
+                    }
+
+                    @Override
+                    public void enableFindingsChanged() {
+                        sendTelemetryOnChange();
+                    }
+
+                    private void sendTelemetryOnChange() {
+                        var newEnabledState = AppMapApplicationSettingsService.getInstance().isAnalysisEnabled();
+                        if (lastEnabledState.getAndSet(newEnabledState) != newEnabledState) {
+                            var eventName = newEnabledState ? "analysis:enable" : "analysis:disable";
+                            TelemetryService.getInstance().sendEvent(eventName);
+                        }
+                    }
+                });
     }
 
     static void openToolWindowAndQuickstart(@NotNull Project project) {
