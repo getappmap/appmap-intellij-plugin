e70a815b4a28053a8418f979c827abb01f62d687
Joachim Ansorg <github@ja-dev.eu>
2023-02-20 12:13:46 +0100

    fix: improve layout of the AppMap tool window to handle space token by collapsed panels

--- plugin-core/src/main/java/appland/toolwindow/AppMapContentPanel.java
+++ plugin-core/src/main/java/appland/toolwindow/AppMapContentPanel.java
@@ -6,10 +6,12 @@ import com.intellij.util.ui.JBUI;
 import javax.swing.*;
 
 public abstract class AppMapContentPanel extends JPanel {
-    public AppMapContentPanel() {
+    public AppMapContentPanel(boolean initialize) {
         super(new VerticalLayout(0));
         setBorder(JBUI.Borders.empty(5, 15, 5, 5));
-        setupPanel();
+        if (initialize) {
+            setupPanel();
+        }
     }
 
     protected abstract void setupPanel();
