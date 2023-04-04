67bc657ce092f2857f831be86ef8bc364c2fe146
Joachim Ansorg <github@ja-dev.eu>
2023-04-04 11:25:43 +0200

    fix: open AppMap for all types of code objects

--- plugin-core/src/main/java/appland/toolwindow/codeObjects/ClassMapItemNavigatable.java
+++ plugin-core/src/main/java/appland/toolwindow/codeObjects/ClassMapItemNavigatable.java
@@ -2,18 +2,15 @@ package appland.toolwindow.codeObjects;
 
 import appland.AppMapBundle;
 import appland.Icons;
-import appland.files.FileLocation;
 import appland.index.AppMapMetadata;
 import appland.index.AppMapMetadataIndex;
 import appland.index.ClassMapItemType;
 import appland.index.ClassMapTypeIndex;
 import com.intellij.openapi.application.ReadAction;
 import com.intellij.openapi.fileEditor.FileEditorManager;
-import com.intellij.openapi.fileEditor.OpenFileDescriptor;
 import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectUtil;
-import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.ui.popup.JBPopupFactory;
 import com.intellij.openapi.ui.popup.PopupStep;
 import com.intellij.openapi.ui.popup.util.BaseListPopupStep;
@@ -33,13 +30,11 @@ public class ClassMapItemNavigatable implements Navigatable {
     private final Project project;
     private final ClassMapItemType type;
     private final String nodeId;
-    private final @Nullable String location;
 
-    public ClassMapItemNavigatable(@NotNull Project project, @NotNull ClassMapItemType type, @NotNull String nodeId, @Nullable String location) {
+    public ClassMapItemNavigatable(@NotNull Project project, @NotNull ClassMapItemType type, @NotNull String nodeId) {
         this.project = project;
         this.type = type;
         this.nodeId = nodeId;
-        this.location = location;
     }
 
     @Override
@@ -54,42 +49,7 @@ public class ClassMapItemNavigatable implements Navigatable {
 
     @Override
     public void navigate(boolean requestFocus) {
-        if (type == ClassMapItemType.Function) {
-            // functions: navigate to source file
-            navigateToSourceFile(requestFocus);
-        } else {
-            // all other types: open the AppMap
-            choseAndNavigateAppMap(requestFocus);
-        }
-    }
-
-    private void navigateToSourceFile(boolean requestFocus) {
-        if (location != null) {
-            var sourceFileWithLocation = ProgressManager.getInstance().runProcessWithProgressSynchronously(() -> {
-                return ReadAction.compute(() -> {
-                    var parsedLocation = FileLocation.parse(location);
-                    if (parsedLocation != null) {
-                        // we're assuming that AppMaps containing the same nodeId are all valid, first is as good as any
-                        var baseFile = ClassMapTypeIndex.findContainingAppMapFiles(project, type, nodeId)
-                                .stream()
-                                .findFirst()
-                                .orElseGet(() -> ProjectUtil.guessProjectDir(project));
-                        if (baseFile != null) {
-                            return Pair.create(parsedLocation.resolveFilePath(project, baseFile), parsedLocation);
-                        }
-                    }
-                    return null;
-                });
-            }, AppMapBundle.get("codeObjects.navigation.locatingSourceFile"), false, project);
-
-            if (sourceFileWithLocation != null) {
-                var line = sourceFileWithLocation.second.getZeroBasedLine(0);
-                new OpenFileDescriptor(project, sourceFileWithLocation.first, line, -1).navigate(requestFocus);
-            } else {
-                Messages.showErrorDialog(AppMapBundle.get("codeObjects.navigation.locationSourceFailed.text"),
-                        AppMapBundle.get("codeObjects.navigation.locationSourceFailed.title"));
-            }
-        }
+        choseAndNavigateAppMap(requestFocus);
     }
 
     @SuppressWarnings("DialogTitleCapitalization")
