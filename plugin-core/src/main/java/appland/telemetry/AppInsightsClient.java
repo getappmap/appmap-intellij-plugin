ab45c6ad6231a7d7073c34de377b8b1f31795b88
Joachim Ansorg <github@ja-dev.eu>
2023-04-21 12:09:06 +0200

    chore: add threading annotations, used shared GSON instance to reduce memory usage

--- plugin-core/src/main/java/appland/telemetry/AppInsightsClient.java
+++ plugin-core/src/main/java/appland/telemetry/AppInsightsClient.java
@@ -1,17 +1,14 @@
 package appland.telemetry;
 
-import com.intellij.openapi.application.ApplicationInfo;
-import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.util.Urls;
-import com.intellij.util.io.HttpRequests;
-
 import appland.AppMapPlugin;
 import appland.telemetry.data.Tag;
 import appland.telemetry.data.TelemetryEvent;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
+import appland.utils.GsonUtils;
+import com.intellij.openapi.application.ApplicationInfo;
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.util.Urls;
+import com.intellij.util.concurrency.annotations.RequiresBackgroundThread;
+import com.intellij.util.io.HttpRequests;
 import org.jetbrains.annotations.NotNull;
 
 
@@ -23,37 +20,37 @@ import org.jetbrains.annotations.NotNull;
  */
 class AppInsightsClient {
     private final @NotNull String ingestionEndpoint;
-    private final @NotNull Gson gson = new GsonBuilder().create();
 
     AppInsightsClient(@NotNull String ingestionEndpoint) {
         this.ingestionEndpoint = ingestionEndpoint;
     }
 
+    @RequiresBackgroundThread
     public void track(@NotNull TelemetryEvent event) {
-        var userId = Identity.getMachineId();
+        var userId = Identity.getOrCreateMachineId();
         var osVersion = System.getProperty("os.version");
 
         event.property("common.os", System.getProperty("os.name"))
-            .property("common.platformversion", System.getProperty("os.version"))
-            .property("common.jvmversion", System.getProperty("java.version"))
-            .property("common.extversion", AppMapPlugin.getDescriptor().getVersion())
-            .property("common.intellijversion", ApplicationInfo.getInstance().getFullVersion())
-            .property("common.product", ApplicationInfo.getInstance().getBuild().getProductCode())
-            .property("common.source", "JetBrains")
-            .tag(Tag.OsVersion, osVersion)
-            .tag(Tag.UserId, userId)
-            .tag(Tag.SessionId, Session.getId());
+                .property("common.platformversion", System.getProperty("os.version"))
+                .property("common.jvmversion", System.getProperty("java.version"))
+                .property("common.extversion", AppMapPlugin.getDescriptor().getVersion())
+                .property("common.intellijversion", ApplicationInfo.getInstance().getFullVersion())
+                .property("common.product", ApplicationInfo.getInstance().getBuild().getProductCode())
+                .property("common.source", "JetBrains")
+                .tag(Tag.OsVersion, osVersion)
+                .tag(Tag.UserId, userId)
+                .tag(Tag.SessionId, Session.getId());
 
         try {
             var url = Urls.newFromEncoded(ingestionEndpoint).resolve("/v2/track").toExternalForm();
-            var json = gson.toJson(event);
+            var json = GsonUtils.GSON.toJson(event);
 
             HttpRequests.post(url, "application/json")
-                .tuner(t -> t.setRequestProperty("Accept", "application/json"))
-                .connect(req -> {
-                    req.write(json);
-                    return req;
-                });
+                    .tuner(t -> t.setRequestProperty("Accept", "application/json"))
+                    .connect(req -> {
+                        req.write(json);
+                        return req;
+                    });
         } catch (Exception e) {
             Logger.getInstance(getClass()).warn(e);
         }
