e18db836b1dde6f7269bcac102e9004493ac9ff0
Joachim Ansorg <github@ja-dev.eu>
2023-04-21 12:08:30 +0200

    chore: clean up Identity, extract method to convert bytes to String, store properties in "getOrCreateMachineId" and not in the method calculating the id

--- plugin-core/src/main/java/appland/telemetry/Identity.java
+++ plugin-core/src/main/java/appland/telemetry/Identity.java
@@ -1,5 +1,10 @@
 package appland.telemetry;
 
+import com.intellij.ide.util.PropertiesComponent;
+import com.intellij.openapi.util.text.StringUtil;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.nio.ByteBuffer;
@@ -10,70 +15,68 @@ import java.util.UUID;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import org.jetbrains.annotations.NotNull;
+class Identity {
+    private static final @NotNull String MachineIdKey = "appland.machine_id";
+    private static final @NotNull Set<String> IgnoredMacAddresses = Set.of("00:00:00:00:00:00", "ff:ff:ff:ff:ff:ff", "ac:de:48:00:11:22");
 
-import com.intellij.ide.util.PropertiesComponent;
+    /**
+     * Retrieves a cached machine id from the application properties.
+     * If there's no value available, then it calculates a new machine id and stores it in the properties.
+     */
+    static @Nullable String getOrCreateMachineId() {
+        var machineId = PropertiesComponent.getInstance().getValue(MachineIdKey);
+        if (StringUtil.isEmpty(machineId)) {
+            machineId = issueMachineId();
+            if (StringUtil.isNotEmpty(machineId)) {
+                PropertiesComponent.getInstance().setValue(MachineIdKey, machineId);
+            }
+        }
+        return machineId;
+    }
 
-public class Identity {
-  @NotNull private final static String MachineIdKey = "appland.machine_id";
-  @NotNull private final static Set<String> IgnoredMacAddresses =
-      Set.of("00:00:00:00:00:00", "ff:ff:ff:ff:ff:ff", "ac:de:48:00:11:22");
+    private static @Nullable String issueMachineId() {
+        // Loop through all the network interfaces and return the first non-loopback
+        // interface that has a hardware address.
+        String machineId = null;
+        try {
+            var networkInterfaces = NetworkInterface.getNetworkInterfaces();
+            while (networkInterfaces.hasMoreElements()) {
+                var networkInterface = networkInterfaces.nextElement();
+                var address = networkInterface.getHardwareAddress();
+                if (address == null || address.length == 0) {
+                    continue;
+                }
 
-  protected static String issueMachineId() {
-    // Loop through all the network interfaces and return the first non-loopback
-    // interface that has a hardware address.
-    String machineId = null;
-    try {
-      var networkInterfaces = NetworkInterface.getNetworkInterfaces();
-      while (networkInterfaces.hasMoreElements()) {
-        var networkInterface = networkInterfaces.nextElement();
-        var address = networkInterface.getHardwareAddress();
-        if (address == null || address.length == 0) {
-          continue;
+                var mac = createHexByteStream(address).collect(Collectors.joining(":"));
+                if (IgnoredMacAddresses.contains(mac)) {
+                    continue;
+                }
+
+                machineId = mac;
+                break;
+            }
+        } catch (SocketException e) {
+            // ignore, we'll generate a random ID as a fallback below
         }
 
-        var buffer = ByteBuffer.wrap(address);
-        String mac = Stream
-          .generate(() -> String.format("%02x", buffer.get()))
-          .limit(buffer.capacity())
-          .collect(Collectors.joining(":"));
-        if (IgnoredMacAddresses.contains(mac)) {
-          continue;
+        if (machineId == null) {
+            machineId = UUID.randomUUID().toString();
         }
 
-        machineId = mac;
-        break;
-      }
-    } catch (SocketException e) {
-      // ignore, we'll generate a random ID as a fallback below
+        try {
+            var hashBytes = MessageDigest.getInstance("SHA-256").digest(machineId.getBytes());
+            return createHexByteStream(hashBytes).collect(Collectors.joining());
+        } catch (NoSuchAlgorithmException e) {
+            return null;
+        }
     }
 
-    if (machineId == null) {
-      machineId = UUID.randomUUID().toString();
+    /**
+     * @param bytes Data to return as hex byte strings
+     * @return Stream of string hex byte representations of the original bytes array
+     */
+    static @NotNull Stream<String> createHexByteStream(byte[] bytes) {
+        var buffer = ByteBuffer.wrap(bytes);
+        return Stream.generate(() -> String.format("%02x", buffer.get())).limit(buffer.capacity());
     }
-
-    String machineIdDigest = null;
-    try {
-      var alg = MessageDigest.getInstance("SHA-256");
-      var hash = ByteBuffer.wrap(alg.digest(machineId.getBytes()));
-
-      machineIdDigest = Stream
-        .generate(() -> String.format("%02x", hash.get()))
-        .limit(hash.capacity())
-        .collect(Collectors.joining());
-    } catch (NoSuchAlgorithmException e) {
-      return "";
-    }
-
-    PropertiesComponent.getInstance().setValue(MachineIdKey, machineIdDigest);
-    return machineIdDigest;
-  }
-
-  public static String getMachineId() {
-    var machineId = PropertiesComponent.getInstance().getValue(MachineIdKey);
-    if (machineId == null || machineId.isEmpty()) {
-      machineId = issueMachineId();
-    }
-    return machineId;
-  }
 }
