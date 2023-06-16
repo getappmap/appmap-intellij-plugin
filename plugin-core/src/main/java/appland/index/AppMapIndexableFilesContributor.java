63a4c0e14bb978be3499e248151b23f8f0c0c4a6
Joachim Ansorg <github@ja-dev.eu>
2023-06-16 20:45:20 +0200

    chore: included all indexed files, e.g. metadata.json, in the indexed files even if they are inside excluded folders

--- plugin-core/src/main/java/appland/index/AppMapIndexableFilesContributor.java
+++ plugin-core/src/main/java/appland/index/AppMapIndexableFilesContributor.java
@@ -1,17 +1,21 @@
 package appland.index;
 
-import appland.files.AppMapFiles;
 import com.intellij.openapi.module.ModuleManager;
+import com.intellij.openapi.progress.ProcessCanceledException;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.ModuleRootManager;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.util.indexing.FileBasedIndexExtension;
 import com.intellij.util.indexing.roots.IndexableFilesContributor;
 import com.intellij.util.indexing.roots.IndexableFilesIterator;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 /**
  * Adds excluded "appmap" folders and excluded AppMap files for indexing.
@@ -34,12 +38,28 @@ public class AppMapIndexableFilesContributor implements IndexableFilesContributo
 
     @Override
     public @NotNull Predicate<VirtualFile> getOwnFilePredicate(@NotNull Project project) {
+        var appMapIndexFilenames = findIndexedFilenames();
         return file -> {
             // we have to include directories, because that controls if the sub-entries are processed
             return file.isDirectory()
-                    || AppMapFiles.isAppMap(file)
                     || ClassMapUtil.isClassMap(file)
-                    || AppMapFindingsUtil.isFindingFile(file);
+                    || AppMapFindingsUtil.isFindingFile(file)
+                    || appMapIndexFilenames.contains(file.getName());
         };
+    }
+
+    @NotNull
+    private static Set<String> findIndexedFilenames() {
+        try {
+            return FileBasedIndexExtension.EXTENSION_POINT_NAME.getExtensionList().stream()
+                    .filter(extension -> extension instanceof AbstractAppMapMetadataFileIndex<?>)
+                    .map(extension -> ((AbstractAppMapMetadataFileIndex<?>) extension).getIndexedFileName())
+                    .collect(Collectors.toSet());
+        } catch (ProcessCanceledException e) {
+            throw e;
+        } catch (Exception e) {
+            // catching any exception, because we must not break indexing
+            return Collections.emptySet();
+        }
     }
 }
