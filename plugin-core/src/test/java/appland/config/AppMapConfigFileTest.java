1e474167fa75b3df7a4f462e67963521ba20b78a
Joachim Ansorg <github@ja-dev.eu>
2023-06-22 11:59:17 +0200

    chore: read and write appmap.yml files with a real Yaml engine instead of string matching

--- plugin-core/src/test/java/appland/config/AppMapConfigFileTest.java
+++ plugin-core/src/test/java/appland/config/AppMapConfigFileTest.java
@@ -5,6 +5,10 @@ import com.intellij.testFramework.fixtures.TempDirTestFixture;
 import com.intellij.testFramework.fixtures.impl.TempDirTestFixtureImpl;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+
 public class AppMapConfigFileTest extends AppMapBaseTest {
     @Override
     protected TempDirTestFixture createTempDirTestFixture() {
@@ -16,7 +20,7 @@ public class AppMapConfigFileTest extends AppMapBaseTest {
         var appMapFile = myFixture.copyFileToProject("appmap-config/appmap.yml");
         var config = AppMapConfigFile.parseConfigFile(appMapFile.toNioPath());
         assertNotNull(config);
-        assertEquals("tmp/appmap", config.appMapDir);
+        assertEquals("tmp/appmap", config.getAppMapDir());
 
         var vfsConfig = AppMapConfigFile.parseConfigFile(appMapFile);
         assertEquals(config, vfsConfig);
@@ -25,8 +29,14 @@ public class AppMapConfigFileTest extends AppMapBaseTest {
     @Test
     public void readConfigWithoutPath() {
         var appMapFile = myFixture.copyFileToProject("appmap-config/appmap-no-dir.yml");
-        assertNull(AppMapConfigFile.parseConfigFile(appMapFile));
-        assertNull(AppMapConfigFile.parseConfigFile(appMapFile.toNioPath()));
+
+        var fromVirtualFile = AppMapConfigFile.parseConfigFile(appMapFile);
+        assertNotNull(fromVirtualFile);
+        assertNull(fromVirtualFile.getAppMapDir());
+
+        var fromNioFile = AppMapConfigFile.parseConfigFile(appMapFile.toNioPath());
+        assertNotNull(fromNioFile);
+        assertNull(fromNioFile.getAppMapDir());
     }
 
     @Test
@@ -34,5 +44,71 @@ public class AppMapConfigFileTest extends AppMapBaseTest {
         var appMapFile = myFixture.copyFileToProject("appmap-config/appmap-empty.yml");
         assertNull(AppMapConfigFile.parseConfigFile(appMapFile));
         assertNull(AppMapConfigFile.parseConfigFile(appMapFile.toNioPath()));
+    }
+
+    @Test
+    public void updateAppMapDirProperty() throws IOException {
+        var appMapFile = myFixture.copyFileToProject("appmap-config/appmap-no-dir.yml");
+        var config = AppMapConfigFile.parseConfigFile(appMapFile);
+        assertNotNull(config);
+        assertNull(config.getAppMapDir());
+
+        // set to text value
+        config.setAppMapDir("/updated/appmap/dir");
+        config.writeTo(appMapFile.toNioPath());
+
+        var updatedConfig = AppMapConfigFile.parseConfigFile(appMapFile.toNioPath());
+        assertNotNull(updatedConfig);
+        assertEquals("/updated/appmap/dir", updatedConfig.getAppMapDir());
+
+        // reset to null
+        config.setAppMapDir(null);
+        config.writeTo(appMapFile.toNioPath());
+
+        updatedConfig = AppMapConfigFile.parseConfigFile(appMapFile.toNioPath());
+        assertNotNull(updatedConfig);
+        assertNull(updatedConfig.getAppMapDir());
+    }
+
+    @Test
+    public void updatePackages() throws IOException {
+        var appMapFile = myFixture.copyFileToProject("appmap-config/appmap-packages.yml");
+        var config = AppMapConfigFile.parseConfigFile(appMapFile);
+        assertNotNull(config);
+
+        var packages = List.of(new AppMapConfigFile.Package("package/one"), new AppMapConfigFile.Package("package/two"));
+        assertEquals(packages, config.getPackages());
+
+        var updatedPackages = List.of(new AppMapConfigFile.Package("new_package/one"), new AppMapConfigFile.Package("new_package/two"));
+        config.setPackages(List.of("new_package/one", "new_package/two"));
+        assertEquals(updatedPackages, config.getPackages());
+
+        config.writeTo(appMapFile.toNioPath());
+        var updatedConfig = AppMapConfigFile.parseConfigFile(appMapFile.toNioPath());
+        assertNotNull(updatedConfig);
+        assertEquals(updatedPackages, config.getPackages());
+
+        // reset packages to empty list
+        config.setPackages(Collections.emptyList());
+        config.writeTo(appMapFile.toNioPath());
+        updatedConfig = AppMapConfigFile.parseConfigFile(appMapFile.toNioPath());
+        assertNotNull(updatedConfig);
+        assertEquals(Collections.emptyList(), updatedConfig.getPackages());
+
+        // reset packages to null
+        config.setPackages(null);
+        config.writeTo(appMapFile.toNioPath());
+        updatedConfig = AppMapConfigFile.parseConfigFile(appMapFile.toNioPath());
+        assertNotNull(updatedConfig);
+        assertEquals(Collections.emptyList(), updatedConfig.getPackages());
+    }
+
+    @Test
+    public void nameProperty() {
+        var config = new AppMapConfigFile();
+        assertNull(config.getName());
+
+        config.setName("my AppMap name");
+        assertEquals("my AppMap name", config.getName());
     }
 }
