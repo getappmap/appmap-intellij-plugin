9f8737a9f91248c04f05446b2a19331b2de027ef
Joachim Ansorg <github@ja-dev.eu>
2023-08-04 14:18:17 +0200

    fix: sync the local filesystem after a run configuration was executed with the AppMap executor

---
+++ plugin-core/src/main/java/appland/execution/AppMapExecutionListener.java
@@ -0,0 +1,24 @@
+package appland.execution;
+
+import com.intellij.execution.ExecutionListener;
+import com.intellij.execution.process.ProcessHandler;
+import com.intellij.execution.runners.ExecutionEnvironment;
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.vfs.VirtualFileManager;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Force a sync of the filesystem to make sure that newly added AppMaps are found.
+ */
+public class AppMapExecutionListener implements ExecutionListener {
+    private static final Logger LOG = Logger.getInstance(AppMapExecutionListener.class);
+
+    @Override
+    public void processTerminated(@NotNull String executorId, @NotNull ExecutionEnvironment env, @NotNull ProcessHandler handler, int exitCode) {
+        if (executorId.equals(AppMapJvmExecutor.EXECUTOR_ID)) {
+            VirtualFileManager.getInstance().asyncRefresh(() -> {
+                LOG.debug("finished filesystem refresh after execution with AppMap");
+            });
+        }
+    }
+}
