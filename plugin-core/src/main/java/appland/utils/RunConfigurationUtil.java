11424996406621af483ae900e6809f38a828f39b
Joachim Ansorg <github@ja-dev.eu>
2023-08-11 12:12:45 +0200

    fix: use read action to fix exception "Read action required" with 2023.2

--- plugin-core/src/main/java/appland/utils/RunConfigurationUtil.java
+++ plugin-core/src/main/java/appland/utils/RunConfigurationUtil.java
@@ -3,6 +3,7 @@ package appland.utils;
 import com.intellij.execution.configurations.ModuleBasedConfiguration;
 import com.intellij.execution.configurations.RunProfile;
 import com.intellij.openapi.module.Module;
+import com.intellij.openapi.project.DumbService;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.ProjectRootManager;
 import com.intellij.openapi.vfs.VirtualFile;
@@ -27,7 +28,9 @@ public final class RunConfigurationUtil {
                                                             @NotNull VirtualFile context) {
         var module = getRunConfigurationModule(runProfile);
         if (module == null) {
-            module = ProjectRootManager.getInstance(project).getFileIndex().getModuleForFile(context, false);
+            module = DumbService.getInstance(project).runReadActionInSmartMode(() -> {
+                return ProjectRootManager.getInstance(project).getFileIndex().getModuleForFile(context, false);
+            });
         }
         if (module == null) {
             throw new IllegalStateException("unable to locate module to store AppMap files");
