3f7e54762b3c2dce2c1dcdf2172b152546766ace
Joachim Ansorg <github@ja-dev.eu>
2023-08-15 14:55:54 +0200

    chore: support appMapMetadata property in finding files

---
+++ plugin-core/src/main/java/appland/problemsView/model/FindingsMetadata.java
@@ -0,0 +1,17 @@
+package appland.problemsView.model;
+
+import com.google.gson.annotations.SerializedName;
+import lombok.EqualsAndHashCode;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * Single metadata value of property "appMapMetadata" of appmap-findings.json files.
+ */
+@EqualsAndHashCode
+public final class FindingsMetadata {
+    @SerializedName("name")
+    public @Nullable String name = null;
+
+    @SerializedName("test_status")
+    public @Nullable TestStatus testStatus = null;
+}
---
+++ plugin-core/src/main/java/appland/problemsView/model/TestStatus.java
@@ -0,0 +1,19 @@
+package appland.problemsView.model;
+
+import com.google.gson.annotations.SerializedName;
+import lombok.Getter;
+import org.jetbrains.annotations.NotNull;
+
+@Getter
+public enum TestStatus {
+    @SerializedName("succeeded")
+    Security("succeeded"),
+    @SerializedName("failed")
+    Failed("failed");
+
+    private final @NotNull String jsonId;
+
+    TestStatus(@NotNull String jsonId) {
+        this.jsonId = jsonId;
+    }
+}
