57cc9a1c2795cb37d499206ee0eaadc5df2209b6
Joachim Ansorg <github@ja-dev.eu>
2023-08-25 15:22:10 +0200

    chore: always attach AppMap agent

--- build.gradle.kts
+++ build.gradle.kts
@@ -143,6 +143,13 @@ allprojects {
             // always execute tests, don't skip by Gradle's up-to-date checks in development
             outputs.upToDateWhen { false }
 
+            // attach AppMap agent
+            dependsOn(":downloadAppMapAgent")
+            jvmArgs("-javaagent:$agentOutputPath",
+                    "-Dappmap.config.file=${rootProject.file("appmap.yml")}",
+                    "-Dappmap.output.directory=${rootProject.buildDir.resolve("appmap")}")
+            systemProperty("appmap.test.withAgent", "true")
+
             // logging setup
             testLogging {
                 setEvents(listOf(TestLogEvent.FAILED, TestLogEvent.STANDARD_OUT, TestLogEvent.STANDARD_ERROR))
