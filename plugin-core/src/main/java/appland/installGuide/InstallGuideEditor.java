0dea54ea0349faeabf0faf89d97868e2b91a489d
Joachim Ansorg <github@ja-dev.eu>
2023-09-06 12:05:21 +0200

    chore: remove unnecessary JSON property

--- plugin-core/src/main/java/appland/installGuide/InstallGuideEditor.java
+++ plugin-core/src/main/java/appland/installGuide/InstallGuideEditor.java
@@ -238,7 +238,6 @@ public class InstallGuideEditor extends WebviewEditor<List<ProjectMetadata>> {
         json.addProperty("page", type.getPageId());
         json.addProperty("userAuthenticated", settings.getApiKey() != null);
         json.addProperty("analysisEnabled", true);
-        json.addProperty("findingsEnabled", true);
     }
 
     private void handleMessageClickLink(@NotNull JsonObject message) {
