010e841a8e4fdca5297e6539190e1f27ef082913
Adam Trotta <ahtrotta@gmail.com>
2023-09-18 16:00:27 -0400

    feat: Update components to 3.1.0 for signin

--- appland-signin/main.js
+++ appland-signin/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue';
-import { VSignIn as plugin } from '@appland/components'; // eslint-disable-line import/no-named-default
+import { default as plugin } from '@appland/components'; // eslint-disable-line import/no-named-default
 
 export {
   mountWebview
