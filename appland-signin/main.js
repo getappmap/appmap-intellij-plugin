7e0e4b7e64e323500da5eed9ea945bb1817b17c6
Adam Trotta <ahtrotta@gmail.com>
2023-09-19 11:35:09 -0400

    Revert "feat: Update components to 3.1.0 for signin"
    This reverts commit 1592fab3b47f24d3f23039b24b17d9933e5ab60c.

--- appland-signin/main.js
+++ appland-signin/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue';
-import { default as plugin } from '@appland/components'; // eslint-disable-line import/no-named-default
+import { VSignIn as plugin } from '@appland/components'; // eslint-disable-line import/no-named-default
 
 export {
   mountWebview
