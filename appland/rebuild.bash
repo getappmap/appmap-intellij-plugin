ef6490768e9d849632f341a0de8cc51c743430ad
Joachim Ansorg <github@ja-dev.eu>
2023-09-29 11:08:44 +0200

    feat: update to latest AppLand JS components

--- appland/rebuild.bash
+++ appland/rebuild.bash
@@ -4,5 +4,8 @@ set -e
 DIR="$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd -P)"
 cd "$DIR"
 
+# https://stackoverflow.com/questions/69692842/error-message-error0308010cdigital-envelope-routinesunsupported
+export NODE_OPTIONS=--openssl-legacy-provider
+
 yarn install
 yarn run bundle
