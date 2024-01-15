45a2d2ae508548635c4eb91f8ba6702cb09f31e4
Joachim Ansorg <github@ja-dev.eu>
2024-01-15 12:05:13 +0100

    chore: fix production build of findings webview and update to latest AppMap components

--- appland-findings/webpack.config.js
+++ appland-findings/webpack.config.js
@@ -1,51 +1,64 @@
 /* eslint-disable */
 const path = require('path');
 
-module.exports = {
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production'
+
+  let config = {
     entry: './main.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js',
-        devtoolModuleFilenameTemplate: '[resource-path]',
-        library: 'AppLandWeb',
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'main.js',
+      devtoolModuleFilenameTemplate: '[resource-path]',
+      library: 'AppLandWeb',
     },
-    devtool: 'source-map',
     resolve: {
-        extensions: ['.js', '.mjs'],
-        alias: {
-            vue: path.resolve('./node_modules/vue'),
-            vuex: path.resolve('./node_modules/vuex'),
-        },
-        fallback: {
-            crypto: 'crypto-js',
-        },
+      extensions: ['.js', '.mjs'],
+      alias: {
+        vue: path.resolve('./node_modules/vue'),
+        vuex: path.resolve('./node_modules/vuex'),
+      },
+      fallback: {
+        crypto: 'crypto-js',
+        assert: false,
+        buffer: false,
+        http: false,
+        https: false,
+      }
     },
     module: {
-        rules: [
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {},
+          },
+        },
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.html$/i,
+          loader: 'html-loader',
+        },
+        {
+          test: /\.(svg|ttf)$/i,
+          use: [
             {
-                test: /\.m?js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {},
-                },
+              loader: 'file-loader',
             },
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.html$/i,
-                loader: 'html-loader',
-            },
-            {
-                test: /\.(svg|ttf)$/i,
-                use: [
-                    {
-                        loader: 'file-loader',
-                    },
-                ],
-            },
-        ],
+          ],
+        },
+      ],
     },
-};
+  }
+
+  if (!isProduction) {
+    config.devtool = 'source-map'
+  }
+
+  return config
+}
