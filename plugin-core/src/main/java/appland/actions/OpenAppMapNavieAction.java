dc08cc7b40e3765834aa8daed9ecf7d763a18590
Joachim Ansorg <github@ja-dev.eu>
2024-01-23 16:19:30 +0100

    feat: Integrate with AppMap Navie

---
+++ plugin-core/src/main/java/appland/actions/OpenAppMapNavieAction.java
@@ -0,0 +1,21 @@
+package appland.actions;
+
+import appland.webviews.navie.NavieEditorProvider;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.project.DumbAware;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Action to open the AppMap Navie webview.
+ */
+@SuppressWarnings("ComponentNotRegistered")
+public class OpenAppMapNavieAction extends AnAction implements DumbAware {
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+        var project = e.getProject();
+        if (project != null) {
+            NavieEditorProvider.openEditor(e.getProject(), e.getDataContext());
+        }
+    }
+}
---
+++ plugin-core/src/main/java/appland/webviews/navie/NavieEditorProvider.java
@@ -0,0 +1,135 @@
+package appland.webviews.navie;
+
+import appland.AppMapBundle;
+import appland.Icons;
+import appland.cli.AppLandCommandLineService;
+import appland.notifications.AppMapNotifications;
+import appland.webviews.WebviewEditorProvider;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.ex.EditorEx;
+import com.intellij.openapi.fileEditor.FileEditor;
+import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.project.ProjectUtil;
+import com.intellij.openapi.ui.popup.JBPopupFactory;
+import com.intellij.openapi.ui.popup.PopupStep;
+import com.intellij.openapi.ui.popup.util.BaseListPopupStep;
+import com.intellij.openapi.util.Key;
+import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.openapi.vfs.VfsUtil;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+
+/**
+ * Open the Navie webview in a new tab.
+ */
+public final class NavieEditorProvider extends WebviewEditorProvider {
+    private static final String TYPE_ID = "appland.navie";
+
+    /**
+     * Default text for the question text input box.
+     */
+    static final Key<String> KEY_QUESTION_TEXT = Key.create("appland.navie.text");
+    /**
+     * The port of the matching indexer's JSON-RPC service.
+     */
+    static final Key<Integer> KEY_INDEXER_RPC_PORT = Key.create("appland.navie.rpcPort");
+
+    public NavieEditorProvider() {
+        super(TYPE_ID);
+    }
+
+    /**
+     * Open a new Navie webview with indexer port and selected derived from the current DataContext.
+     * If no indexer port could be found for the context, an error message is displayed.
+     *
+     * @param project Current project
+     * @param context Data context to locate matching indexer and selected text, if available.
+     */
+    public static void openEditor(@NotNull Project project, @NotNull DataContext context) {
+        var provider = WebviewEditorProvider.findEditorProvider(TYPE_ID);
+        assert provider != null;
+
+        var editor = CommonDataKeys.EDITOR_EVEN_IF_INACTIVE.getData(context);
+        chooseIndexerJsonRpcPort(project, context, editor, (indexerPort, selectedText) -> {
+            if (indexerPort == null) {
+                AppMapNotifications.showNavieUnavailableNotification(project);
+                return;
+            }
+
+            var file = provider.createVirtualFile(AppMapBundle.get("webview.navie.title"));
+            KEY_INDEXER_RPC_PORT.set(file, indexerPort);
+            KEY_QUESTION_TEXT.set(file, selectedText);
+
+            FileEditorManager.getInstance(project).openFile(file, true);
+        });
+    }
+
+    private static void chooseIndexerJsonRpcPort(@NotNull Project project,
+                                                 @NotNull DataContext context,
+                                                 @Nullable Editor editor,
+                                                 @NotNull ShowNavieConsumer consumer) {
+        var service = AppLandCommandLineService.getInstance();
+        var selectedText = editor != null ? editor.getSelectionModel().getSelectedText() : null;
+
+        var contextFile = editor instanceof EditorEx ? ((EditorEx) editor).getVirtualFile() : null;
+        if (contextFile != null) {
+            var port = service.getIndexerRpcPort(contextFile);
+            if (port != null) {
+                consumer.openNavie(port, selectedText);
+                return;
+            }
+        }
+
+        var activeRoots = service.getActiveRoots();
+        if (activeRoots.isEmpty()) {
+            consumer.openNavie(null, null);
+            return;
+        }
+
+        // without a context file, fallback to the only available indexer port if there's exactly one active root
+        if (activeRoots.size() == 1) {
+            consumer.openNavie(service.getIndexerRpcPort(activeRoots.get(0)), selectedText);
+            return;
+        }
+
+        // for multiple active roots, let the user choose one
+        var selectPortStep = new BaseListPopupStep<>(AppMapBundle.get("webview.navie.chooseAppMapModule.title"), activeRoots) {
+            @Override
+            public @NotNull String getTextFor(VirtualFile file) {
+                var projectDir = ProjectUtil.guessProjectDir(project);
+                var relativePath = projectDir != null ? VfsUtil.getRelativePath(file, projectDir) : null;
+                return StringUtil.defaultIfEmpty(relativePath, file.getPresentableUrl());
+            }
+
+            @Override
+            public @Nullable PopupStep<?> onChosen(VirtualFile selectedValue, boolean finalChoice) {
+                var port = selectedValue != null ? service.getIndexerRpcPort(selectedValue) : null;
+                consumer.openNavie(port, selectedText);
+                return FINAL_CHOICE;
+            }
+        };
+
+        JBPopupFactory.getInstance().createListPopup(selectPortStep).showCenteredInCurrentWindow(project);
+    }
+
+    @Override
+    public @NotNull FileEditor createEditor(@NotNull Project project, @NotNull VirtualFile file) {
+        return new NavieEditor(project, file);
+    }
+
+    @Override
+    public Icon getEditorIcon() {
+        return Icons.APPMAP_FILE;
+    }
+
+    @FunctionalInterface
+    private interface ShowNavieConsumer {
+        void openNavie(@Nullable Integer indexerPort, @Nullable String selectedText);
+    }
+}
