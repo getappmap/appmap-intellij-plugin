15b9bd1c1444108d5eadcdc96f7c7d924f20d5a4
Joachim Ansorg <github@ja-dev.eu>
2024-02-13 17:55:49 +0100

    fix: retrieve Gradle JDK in a background thread

--- plugin-gradle/src/main/java/appland/execution/AppMapGradleAgentRunner.java
+++ plugin-gradle/src/main/java/appland/execution/AppMapGradleAgentRunner.java
@@ -1,5 +1,6 @@
 package appland.execution;
 
+import appland.AppMapBundle;
 import com.intellij.execution.ExecutionException;
 import com.intellij.execution.configurations.RunProfile;
 import com.intellij.execution.configurations.RunnerSettings;
@@ -8,9 +9,14 @@ import com.intellij.execution.runners.ProgramRunner;
 import com.intellij.openapi.externalSystem.service.execution.ExternalSystemJdkUtil;
 import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil;
 import com.intellij.openapi.externalSystem.util.ExternalSystemConstants;
+import com.intellij.openapi.progress.ProgressIndicator;
+import com.intellij.openapi.progress.Task;
 import com.intellij.openapi.project.DumbService;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.projectRoots.Sdk;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.jetbrains.plugins.gradle.service.execution.GradleRunConfiguration;
 import org.jetbrains.plugins.gradle.settings.GradleSettings;
 import org.jetbrains.plugins.gradle.util.GradleConstants;
@@ -44,14 +50,40 @@ public class AppMapGradleAgentRunner implements ProgramRunner<RunnerSettings> {
     private static void verifyJdk(@NotNull ExecutionEnvironment environment) throws ExecutionException {
         var manager = ExternalSystemApiUtil.getManager(GradleConstants.SYSTEM_ID);
         if (manager != null) {
+            var project = environment.getProject();
             var gradleProjectPath = ((GradleRunConfiguration) environment.getRunProfile()).getSettings().getExternalProjectPath();
-            var gradleProjectSettings = GradleSettings.getInstance(environment.getProject()).getLinkedProjectSettings(gradleProjectPath);
+            var gradleProjectSettings = GradleSettings.getInstance(project).getLinkedProjectSettings(gradleProjectPath);
             if (gradleProjectSettings != null) {
-                var jdk = ExternalSystemJdkUtil.getJdk(environment.getProject(), gradleProjectSettings.getGradleJvm());
+                var jdk = getGradleSdkWithProgress(project, gradleProjectSettings.getGradleJvm());
                 if (jdk != null) {
-                    AppMapJvmExecutor.verifyJDK(environment.getProject(), jdk);
+                    AppMapJvmExecutor.verifyJDK(project, jdk);
                 }
             }
+        }
+    }
+
+    /**
+     * Because @{link {@link ExternalSystemJdkUtil#getJdk(Project, String)}} is a SlowOperation in 2023.2,
+     * we need to move this into a background thread.
+     * Refer to https://github.com/getappmap/appmap-intellij-plugin/issues/566 for a stacktrace.
+     *
+     * @return The Gradle JVM SDK, if available.
+     */
+    private static @Nullable Sdk getGradleSdkWithProgress(@NotNull Project project,
+                                                          @Nullable String gradleJvmName) {
+        var title = AppMapBundle.get("appMapExecutor.validatingGradleJDK");
+        var task = new Task.WithResult<Sdk, Exception>(project, title, true) {
+            @Override
+            protected Sdk compute(@NotNull ProgressIndicator indicator) {
+                return ExternalSystemJdkUtil.getJdk(project, gradleJvmName);
+            }
+        };
+        task.queue();
+
+        try {
+            return task.getResult();
+        } catch (Exception e) {
+            return null;
         }
     }
 }
