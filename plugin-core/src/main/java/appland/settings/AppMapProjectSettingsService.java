e10c4398cf3e004a12811f00b4d0b4964c86f87f
Joachim Ansorg <github@ja-dev.eu>
2024-04-10 16:22:38 +0200

    chore: use @Service annotation where applicable

--- plugin-core/src/main/java/appland/settings/AppMapProjectSettingsService.java
+++ plugin-core/src/main/java/appland/settings/AppMapProjectSettingsService.java
@@ -1,11 +1,13 @@
 package appland.settings;
 
 import com.intellij.openapi.components.PersistentStateComponent;
+import com.intellij.openapi.components.Service;
 import com.intellij.openapi.components.State;
 import com.intellij.openapi.components.Storage;
 import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.NotNull;
 
+@Service(Service.Level.PROJECT)
 @State(name = "appmap-project", storages = @Storage("appmap.xml"))
 public final class AppMapProjectSettingsService implements PersistentStateComponent<AppMapProjectSettings> {
     @NotNull
