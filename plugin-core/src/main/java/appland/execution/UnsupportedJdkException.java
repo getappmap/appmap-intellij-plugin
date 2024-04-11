23af3f17c4e1f418248df2588acc9d641f9549fb
Joachim Ansorg <github@ja-dev.eu>
2024-04-11 15:11:27 +0200

    chore: drop code used for <= 2023.1

--- plugin-core/src/main/java/appland/execution/UnsupportedJdkException.java
+++ plugin-core/src/main/java/appland/execution/UnsupportedJdkException.java
@@ -34,10 +34,6 @@ public class UnsupportedJdkException extends ExecutionException implements Hyper
 
     @NotNull
     private static String findMessage() {
-        // 2021.3 and earlier don't support link handlers of ExecutionException when the exception is shown as message dialog.
-        // 2022.1 and later display a notification instead of a message dialog and thus support the link handler.
-        return ApplicationInfo.getInstance().getBuild().getBaselineVersion() <= 213
-                ? AppMapBundle.get("appMapExecutor.jdkNotSupported")
-                : AppMapBundle.get("appMapExecutor.jdkNotSupportedWithLink");
+        return AppMapBundle.get("appMapExecutor.jdkNotSupportedWithLink");
     }
 }
