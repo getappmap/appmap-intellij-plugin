5f4c4436c3458c6053071be8fd8febec9e1df1b3
Joachim Ansorg <github@ja-dev.eu>
2024-04-16 12:14:21 +0200

    fix: append AppMap argline to an existing argline

--- plugin-maven/src/main/java/appland/execution/AppMapMavenProgramPatcher.java
+++ plugin-maven/src/main/java/appland/execution/AppMapMavenProgramPatcher.java
@@ -2,6 +2,7 @@ package appland.execution;
 
 import com.intellij.execution.configurations.JavaParameters;
 import com.intellij.execution.configurations.RunProfile;
+import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.util.text.Strings;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.idea.maven.execution.MavenRunConfiguration;
@@ -20,6 +21,12 @@ public class AppMapMavenProgramPatcher extends AbstractAppMapJavaProgramPatcher 
 
         // delegate to Maven child processes via argLine. It follows the implementation of the Maven surefire plugin
         // https://github.com/apache/maven-surefire/blob/78805045bb90d7cc5692b6a388e3605d648146d2/surefire-its/src/test/java/org/apache/maven/surefire/its/fixture/SurefireLauncher.java#L372
-        javaParameters.getVMParametersList().add("-DargLine=" + Strings.join(jvmParams, " "));
+        var existingArgLine = StringUtil.defaultIfEmpty(javaParameters.getVMParametersList().getPropertyValue("argLine"), "");
+        var appMapArgLine = Strings.join(jvmParams, " ");
+        if (!existingArgLine.isEmpty()) {
+            appMapArgLine = existingArgLine + " " + appMapArgLine;
+        }
+
+        javaParameters.getVMParametersList().add("-DargLine=" + appMapArgLine);
     }
 }
