2c46fbd15a04fda5aa71044e653516a747e0b9f0
Joachim Ansorg <github@ja-dev.eu>
2024-04-29 15:11:34 +0200

    chore: support optional directory for "open-location"

--- appland-navie/webview.js
+++ appland-navie/webview.js
@@ -62,10 +62,12 @@ export function mountWebview() {
     });
 
     app.$on('open-install-instructions', () => vscode.postMessage({command: 'open-install-instructions'}))
-    app.$on('open-location', (path) => vscode.postMessage({command: 'open-location', path}))
     app.$on('open-record-instructions', () => vscode.postMessage({command: 'open-record-instructions'}))
     app.$on('open-appmap', (path) => vscode.postMessage({command: 'open-appmap', path}))
     app.$on('show-appmap-tree', () => vscode.postMessage({command: 'show-appmap-tree'}));
+    app.$on('open-location', (path, directory) => {
+      return vscode.postMessage({command: 'open-location', path, directory});
+    })
   });
 
   vscode.postMessage({ command: "ready" });
