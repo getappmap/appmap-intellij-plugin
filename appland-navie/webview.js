2b71585bdb9d97b2c25ff7e09e75dc3d1454b89c
Adam Trotta <ahtrotta@gmail.com>
2024-05-01 11:40:46 -0400

    feat: Update Navie @appland/components to 4.22.0

--- appland-navie/webview.js
+++ appland-navie/webview.js
@@ -24,6 +24,7 @@ export function mountWebview() {
             appmapYmlPresent: this.appmapYmlPresent,
             targetAppmapData: initialData.targetAppmapData,
             targetAppmapFsPath: initialData.targetAppmapFsPath,
+            disableLlmConfig: true,
           },
         });
       },
