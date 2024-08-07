66dc130a32e02551387378b38521ee6e9ed6594b
Joachim Ansorg <github@ja-dev.eu>
2024-08-07 11:27:01 +0200

    chore: don't refresh for projects in unit test mode

--- plugin-core/src/main/java/appland/cli/AppLandProjectManagerListener.java
+++ plugin-core/src/main/java/appland/cli/AppLandProjectManagerListener.java
@@ -1,5 +1,6 @@
 package appland.cli;
 
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManagerListener;
 import org.jetbrains.annotations.NotNull;
@@ -11,6 +12,12 @@ import org.jetbrains.annotations.NotNull;
 public class AppLandProjectManagerListener implements ProjectManagerListener {
     @Override
     public void projectClosed(@NotNull Project project) {
+        // We're not launching the CLI processes by default in test mode,
+        // because it's async and may interfere with other tests.
+        if (ApplicationManager.getApplication().isUnitTestMode()) {
+            return;
+        }
+
         AppLandCommandLineService.getInstance().refreshForOpenProjectsInBackground();
     }
 }
