a9a2fba323a46c4189270cc9df8f9c0093f418a3
Joachim Ansorg <github@ja-dev.eu>
2024-08-12 11:47:23 +0200

    core: don't reload findings in unit tests

--- plugin-core/src/main/java/appland/startup/AppLandStartupActivity.java
+++ plugin-core/src/main/java/appland/startup/AppLandStartupActivity.java
@@ -1,6 +1,7 @@
 package appland.startup;
 
 import appland.problemsView.FindingsManager;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.startup.StartupActivity;
 import org.jetbrains.annotations.NotNull;
@@ -8,6 +9,12 @@ import org.jetbrains.annotations.NotNull;
 public class AppLandStartupActivity implements StartupActivity {
     @Override
     public void runActivity(@NotNull Project project) {
+        // Don't reload in unit tests because the reload is async.
+        // AppMapBaseTest reset all findings before each test, anyway.
+        if (ApplicationManager.getApplication().isUnitTestMode()) {
+            return;
+        }
+
         // load initial findings of the project
         FindingsManager.getInstance(project).reloadAsync();
     }
