3bdc6c83f9a03e38cc734f20b1c47e8c11df8edc
Dustin Byrne <dustinsbyrne@gmail.com>
2024-09-12 18:05:28 -0400

    feat: Update @appland/components to v4.36.0
    This introduces the 'next step' buttons. This change also moves away
    from webpack in favor of tsup due to the introduction of upstream
    dependency issues.

--- appland-navie/main.js
+++ appland-navie/main.js
@@ -1,8 +1,6 @@
-import Vue from 'vue';
-import { default as plugin } from '@appland/components'; // eslint-disable-line import/no-named-default
-
-export {
-  mountWebview
-} from './webview';
+import Vue from "vue";
+import { default as plugin } from "@appland/components"; // eslint-disable-line import/no-named-default
+import { mountWebview } from "./webview";
 
 Vue.use(plugin);
+mountWebview();
