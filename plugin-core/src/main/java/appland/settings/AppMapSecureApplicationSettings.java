569a5c594db0f41e96057a86f56c3f5fbcf56cc4
Joachim Ansorg <github@ja-dev.eu>
2025-04-02 11:49:49 +0200

    chore: refactor secure application settings to cache more than one setting at a time

--- plugin-core/src/main/java/appland/settings/AppMapSecureApplicationSettings.java
+++ plugin-core/src/main/java/appland/settings/AppMapSecureApplicationSettings.java
@@ -3,10 +3,21 @@ package appland.settings;
 import com.intellij.openapi.util.text.StringUtil;
 import org.jetbrains.annotations.Nullable;
 
+/**
+ * Application settings, which are securely stored in the {@link com.intellij.ide.passwordSafe.PasswordSafe}.
+ */
 public interface AppMapSecureApplicationSettings {
-    @Nullable
-    String getOpenAIKey();
+    /**
+     * The key used to store the OpenAI key in the password safe.
+     * If possible, this method should be called from a background thread.
+     */
+    @Nullable String getOpenAIKey();
 
+    /**
+     * Updates the value of the OpenAI key in the password safe.
+     *
+     * @param key New value of the OpenAI key.
+     */
     void setOpenAIKey(@Nullable String key);
 
     default boolean hasOpenAIKey() {
--- plugin-core/src/test/java/appland/settings/AppMapSecureApplicationSettingsServiceTest.java
+++ plugin-core/src/test/java/appland/settings/AppMapSecureApplicationSettingsServiceTest.java
@@ -1,11 +1,14 @@
 package appland.settings;
 
 import appland.AppMapBaseTest;
+import com.intellij.openapi.application.ApplicationManager;
 import org.junit.Test;
 
 public class AppMapSecureApplicationSettingsServiceTest extends AppMapBaseTest {
     @Test
     public void openAiKey() {
+        ApplicationManager.getApplication().assertIsDispatchThread();
+
         var settings = AppMapSecureApplicationSettingsService.getInstance();
 
         // if executed on the EDT, it must not throw an exception about slow operations
