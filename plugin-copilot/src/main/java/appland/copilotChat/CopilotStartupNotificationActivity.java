45187f2a526b452b605dc7e0e221c06317536153
Joachim Ansorg <github@ja-dev.eu>
2025-05-15 10:40:38 +0200

    fix: don't show multiple notifications about Copilot after a project is opened Previously, one notification for each opened project was shown in each project window.

--- plugin-copilot/src/main/java/appland/copilotChat/CopilotStartupNotificationActivity.java
+++ plugin-copilot/src/main/java/appland/copilotChat/CopilotStartupNotificationActivity.java
@@ -2,7 +2,6 @@ package appland.copilotChat;
 
 import appland.ProjectActivityAdapter;
 import appland.copilotChat.copilot.GitHubCopilotService;
-import appland.notifications.AppMapNotifications;
 import appland.settings.AppMapApplicationSettingsService;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.DumbAware;
@@ -10,6 +9,9 @@ import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.concurrent.atomic.AtomicBoolean;
+
+import static appland.notifications.AppMapNotifications.showCopilotAuthenticationRequired;
+import static appland.notifications.AppMapNotifications.showFirstCopilotIntegrationEnabled;
 
 public class CopilotStartupNotificationActivity extends ProjectActivityAdapter implements DumbAware {
     private static final AtomicBoolean isNotificationShown = new AtomicBoolean(false);
@@ -27,19 +29,17 @@ public class CopilotStartupNotificationActivity extends ProjectActivityAdapter i
         }
 
         // if the Copilot integration is enabled, but not authenticated, show a notification
-        GitHubCopilotService copilotService = GitHubCopilotService.getInstance();
+        var copilotService = GitHubCopilotService.getInstance();
         if (!copilotService.isCopilotAuthenticated()) {
-            ApplicationManager.getApplication().invokeLater(AppMapNotifications::showCopilotAuthenticationRequired);
+            ApplicationManager.getApplication().invokeLater(() -> showCopilotAuthenticationRequired(project));
             return;
         }
 
         // if the Copilot integration is enabled for the first time, show a notification
-        var wasSeenBefore = AppMapApplicationSettingsService.getInstance().isCopilotIntegrationDetected();
-        if (!wasSeenBefore) {
-            if (isNotificationShown.compareAndExchange(false, true)) {
-                AppMapApplicationSettingsService.getInstance().setCopilotIntegrationDetected(true);
-                ApplicationManager.getApplication().invokeLater(AppMapNotifications::showFirstCopilotIntegrationEnabled);
-            }
+        var wasDetectedBefore = AppMapApplicationSettingsService.getInstance().isCopilotIntegrationDetected();
+        if (!wasDetectedBefore && isNotificationShown.compareAndExchange(false, true)) {
+            AppMapApplicationSettingsService.getInstance().setCopilotIntegrationDetected(true);
+            ApplicationManager.getApplication().invokeLater(() -> showFirstCopilotIntegrationEnabled(project));
         }
 
         try {
