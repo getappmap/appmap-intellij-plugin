c061260fa955c11f1927dcbe6c8ececd658fc3fa
Rafał Rzepecki <divided.mind@gmail.com>
2025-07-01 18:35:08 +0200

    feat: Implement quick review command
    Adds a new command `appmap.quickReview` that allows users to select a Git
    reference (branch, tag, or commit) as a base and initiate an AI-driven code
    review of the changes relative to that base via Navie. Integrates the command
    into the Navie tool window and the menu.

--- plugin-core/src/main/java/appland/toolwindow/navie/NaviePanel.java
+++ plugin-core/src/main/java/appland/toolwindow/navie/NaviePanel.java
@@ -1,11 +1,12 @@
 package appland.toolwindow.navie;
 
 import appland.AppMapBundle;
+import appland.actions.QuickReviewAction;
 import appland.toolwindow.AppMapContentPanel;
 import appland.webviews.navie.NavieEditorProvider;
 import com.intellij.ide.plugins.newui.ColorButton;
 import com.intellij.openapi.Disposable;
-import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.editor.ex.util.EditorUtil;
 import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.project.Project;
@@ -28,26 +29,13 @@ public class NaviePanel extends AppMapContentPanel {
     protected void setupPanel() {
         add(newWrappingLabel(AppMapBundle.get("toolwindow.appmap.navie.intro"), false));
         add(createNavieChatButton());
+        add(createQuickReviewButton());
         add(newWrappingLabel(AppMapBundle.get("toolwindow.appmap.navie.details"), true));
     }
 
-    private @NotNull JButton createNavieChatButton() {
-        // anonymous class because setBgColor and the related methods have protected visibility
+    private @NotNull JButton createButton() {
         return new ColorButton() {
             {
-                setText(AppMapBundle.get("toolwindow.appmap.navie.newNavieChat"));
-
-                addActionListener(e -> {
-                    // Provide context of current editor, even if it's not in focus.
-                    // We want to provide the selection of the editor to Navie.
-                    var currentEditor = FileEditorManager.getInstance(project).getSelectedTextEditor();
-                    var editorContext = currentEditor != null
-                            ? EditorUtil.getEditorDataContext(currentEditor)
-                            : DataContext.EMPTY_CONTEXT;
-
-                    NavieEditorProvider.openEditor(project, editorContext);
-                });
-
                 // We're using theme colors of similar UI to choose matching colors
                 setBgColor(JBUI.CurrentTheme.GotItTooltip.background(true));
                 setTextColor(JBUI.CurrentTheme.GotItTooltip.foreground(true));
@@ -56,6 +44,34 @@ public class NaviePanel extends AppMapContentPanel {
                 setFocusedBorderColor(JBUI.CurrentTheme.Focus.focusColor());
             }
         };
+    }
+
+    private @NotNull JButton createNavieChatButton() {
+        var button = createButton();
+        button.setText(AppMapBundle.get("toolwindow.appmap.navie.newNavieChat"));
+        button.addActionListener(e -> {
+            // Provide context of current editor, even if it's not in focus.
+            // We want to provide the selection of the editor to Navie.
+            var currentEditor = FileEditorManager.getInstance(project).getSelectedTextEditor();
+            var editorContext = currentEditor != null
+                    ? EditorUtil.getEditorDataContext(currentEditor)
+                    : DataContext.EMPTY_CONTEXT;
+
+            NavieEditorProvider.openEditor(project, editorContext);
+        });
+        return button;
+    }
+
+    private @NotNull JButton createQuickReviewButton() {
+        var button = createButton();
+        button.setText(AppMapBundle.get("toolwindow.appmap.navie.quickReview"));
+        button.addActionListener(e -> {
+            var manager = ActionManager.getInstance();
+            var action = manager.getAction(QuickReviewAction.ACTION_ID);
+            assert (action != null);
+            manager.tryToExecute(action, null, this, ActionPlaces.TOOLWINDOW_CONTENT, true);
+        });
+        return button;
     }
 
     private static @NotNull JBLabel newWrappingLabel(@NotNull String text, boolean topPadding) {
