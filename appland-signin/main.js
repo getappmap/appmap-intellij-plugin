dd6489c83b39106c0c73639748714cdade253d68
Rafał Rzepecki <divided.mind@gmail.com>
2025-07-08 15:41:47 +0200

    refactor: Merge the webview code into a single directory
    This is both for ease of maintainance and to reduce the size of
    the plugin. Yarn is migrated to 4.1.3 and added to the repository;
    webview update CI workflow is also removed (it appears to never
    have been used).

--- appland-signin/main.js
+++
@@ -1,6 +0,0 @@
-import Vue from "vue";
-import { default as plugin } from "@appland/components"; // eslint-disable-line import/no-named-default
-import { mountWebview } from "./webview";
-
-Vue.use(plugin);
-mountWebview();
