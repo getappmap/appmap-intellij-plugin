728e3a7a1664d3043d46ecc756776146c9b42a57
Joachim Ansorg <github@ja-dev.eu>
2025-10-10 12:17:58 +0200

    feat: show the deployment settings in the AppMap status report

--- plugin-core/src/main/java/appland/telemetry/SplunkTelemetryUtils.java
+++ plugin-core/src/main/java/appland/telemetry/SplunkTelemetryUtils.java
@@ -1,6 +1,7 @@
 package appland.telemetry;
 
 import appland.deployment.AppMapDeploymentSettings;
+import com.intellij.openapi.util.text.StringUtil;
 import org.jetbrains.annotations.Nullable;
 
 public class SplunkTelemetryUtils {
@@ -8,5 +9,12 @@ public class SplunkTelemetryUtils {
 
     public static boolean isSplunkTelemetryEnabled(@Nullable AppMapDeploymentSettings settings) {
         return settings != null && settings.getTelemetry() != null && SPLUNK_BACKEND_ID.equals(settings.getTelemetry().getBackend());
+    }
+
+    public static boolean isSplunkTelemetryActive(@Nullable AppMapDeploymentSettings settings) {
+        return isSplunkTelemetryEnabled(settings)
+                && settings.getTelemetry() != null
+                && StringUtil.isNotEmpty(settings.getTelemetry().getUrl())
+                && StringUtil.isNotEmpty(settings.getTelemetry().getToken());
     }
 }
