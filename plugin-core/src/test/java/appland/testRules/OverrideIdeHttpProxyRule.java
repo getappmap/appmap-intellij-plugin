520f26845f2b0cd2c15f66c5075e9a22b280297a
Rafał Rzepecki <divided.mind@gmail.com>
2026-06-02 16:30:03 +0200

    test: Migrate from HttpConfigurable to ProxySettings in proxy test
    HttpConfigurable is deprecated; use ProxySettings (the recommended
    replacement) instead.

--- plugin-core/src/test/java/appland/testRules/OverrideIdeHttpProxyRule.java
+++ plugin-core/src/test/java/appland/testRules/OverrideIdeHttpProxyRule.java
@@ -1,6 +1,7 @@
 package appland.testRules;
 
-import com.intellij.util.net.HttpConfigurable;
+import com.intellij.util.net.ProxyConfiguration;
+import com.intellij.util.net.ProxySettings;
 import org.jetbrains.annotations.NotNull;
 import org.junit.rules.TestRule;
 import org.junit.runner.Description;
@@ -19,21 +20,15 @@ public final class OverrideIdeHttpProxyRule implements TestRule {
         return new Statement() {
             @Override
             public void evaluate() throws Throwable {
-                var settings = HttpConfigurable.getInstance();
-                var wasHttpProxy = settings.USE_HTTP_PROXY;
-                var wasProxyHost = settings.PROXY_HOST;
-                var wasProxyPort = settings.PROXY_PORT;
+                var settings = ProxySettings.getInstance();
+                var originalConfig = settings.getProxyConfiguration();
 
                 try {
-                    settings.USE_HTTP_PROXY = true;
-                    settings.PROXY_HOST = "127.0.0.1";
-                    settings.PROXY_PORT = mockServerRule.getPort();
+                    settings.setProxyConfiguration(ProxyConfiguration.proxy(ProxyConfiguration.ProxyProtocol.HTTP, "127.0.0.1", mockServerRule.getPort(), ""));
 
                     statement.evaluate();
                 } finally {
-                    settings.USE_HTTP_PROXY = wasHttpProxy;
-                    settings.PROXY_HOST = wasProxyHost;
-                    settings.PROXY_PORT = wasProxyPort;
+                    settings.setProxyConfiguration(originalConfig);
                 }
             }
         };
